200963331ecb0ebe1901abaf26d93e0f1893d2d8
calebgl <calebguerrero13@gmail.com>
2022-02-25 16:31:20 -0600

    Data normalized

---
+++ Práctica 02/main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import json
+
+
+def read_raw_data(file_name: str) -> list[pd.DataFrame]:
+    raw_data_frame = pd.read_csv(file_name)
+    return raw_data_frame
+
+
+def read_categories(file_name: str) -> dict[int, str]:
+    with open(file_name, "r") as file:
+        json_data = json.load(file)
+        return {
+            int(category["id"]): category["snippet"]["title"]
+            for category in json_data["items"]
+        }
+
+
+def insert_categories(data_frame: list[pd.DataFrame], categories: object) -> None:
+    data_frame["category"] = data_frame["categoryId"].map(categories)
+    data_frame.drop(["categoryId"], axis=1)
+
+
+def normalize_date(data_frame: list[pd.DataFrame], columns: list[str]) -> None:
+    for column in columns:
+        data_frame[column] = pd.to_datetime(data_frame[column])
+
+
+def main() -> None:
+    data_frame = read_raw_data("MX_youtube_trending_data.csv")
+    categories = read_categories("MX_category_id.json")
+    date_columns = ["publishedAt", "trending_date"]
+
+    insert_categories(data_frame, categories)
+    normalize_date(data_frame, date_columns)
+
+    print(data_frame)
+
+
+if __name__ == "__main__":
+    main()
